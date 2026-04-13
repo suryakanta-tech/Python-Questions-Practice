ec0bb47f24c5a20a216ecc68f4db9399c90f65d8
suryakanta-tech <253405826+suryakanta-tech@users.noreply.github>
2026-04-13 09:01:24 +0530

    Python List Problem-8

---
+++ Python List/problem-8.py
@@ -0,0 +1,9 @@
+# 8.Merge two lists into one.
+x1 = ['surya','basu','kanta']
+x2 = [20,30,40]
+x1.extend(x2)
+print(x1)
+# or
+x1 = ['surya','basu','kanta']
+x2 = [20,30,40]
+print(x1 + x2)  # using '+'
